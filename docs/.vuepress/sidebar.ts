9c0d58f704bbd7fa8171625871f2abbf896bbc83
Astrobiology-cn <169008688+Astrobiology-cn@users.noreply.github.com>
2025-02-14 20:14:33 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -8,6 +8,7 @@ export default sidebar({
     "/Fitness",
     "/BioinorganicChemistry.zh",
     "/testdemo",
+    "/uploaddemo",
     // 读书笔记架构更换到 docsify，不能使用相对链接
     { text: "读书笔记", icon: "fa6-brands:readme", link: "https://newzone.top/reading/" },
     // 指定显示页面
